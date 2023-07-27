9cff8aeefab559aa7d919f9b4e57e6c1e8fc2c0d
noura2tarek <Nourat536@gmail.com>
2023-07-27 19:14:07 +0300

    themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:hexcolor/hexcolor.dart';
 import 'package:news_app/Layout/news_layout.dart';
 import 'package:news_app/Network/Local/cache_helper.dart';
 import 'package:news_app/Network/Remote/dio_helper.dart';
 import 'package:news_app/Shared/Bloc/cubit.dart';
 import 'package:news_app/Shared/Bloc/states.dart';
+import 'package:news_app/Styles/themes.dart';
 
 import 'Shared/bloc_observer.dart';
 
@@ -36,104 +35,8 @@ class MyApp extends StatelessWidget {
             debugShowCheckedModeBanner: false,
             //theme parameter executed when the theme mode is light
             //dark theme will executed when the theme mode is dark
-            theme: ThemeData(
-              dividerTheme: DividerThemeData(
-                color: Colors.grey[400],
-              ),
-              primarySwatch: Colors.indigo,
-              primaryColor: Colors.indigo,
-              scaffoldBackgroundColor: Colors.white,
-              textTheme: const TextTheme(
-                  bodySmall: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18.0,
-                    color: Colors.black,
-                  ),
-              ),
-              appBarTheme: const AppBarTheme(
-                titleSpacing: 16.0,
-                  titleTextStyle: TextStyle(
-                    color: Colors.black,
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  iconTheme: IconThemeData(
-                    color: Colors.black,
-                  ),
-                  backgroundColor: Colors.white,
-                  elevation: 0.0,
-                  systemOverlayStyle: SystemUiOverlayStyle(
-                    statusBarColor: Colors.white,
-                    statusBarIconBrightness: Brightness.dark,
-                  )
-              ),
-              bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-                type: BottomNavigationBarType.fixed,
-                showUnselectedLabels: true,
-                showSelectedLabels: true,
-                elevation: 20.0,
-                backgroundColor: Colors.white,
-              ),
-
-              //colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-              //useMaterial3: true,
-            ),
-            darkTheme: ThemeData(
-              primarySwatch: Colors.grey,
-              dividerTheme: DividerThemeData(
-                color: Colors.grey[200],
-              ),
-              inputDecorationTheme: const InputDecorationTheme(
-                hintStyle: TextStyle(
-                  color: Colors.white,
-                ),
-                labelStyle: TextStyle(
-                  color: Colors.white,
-                ),
-                prefixIconColor: Colors.white,
-                fillColor: Colors.white,
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.grey,
-                  ),
-                ),
-
-              ),
-              scaffoldBackgroundColor: HexColor('333739'),
-
-              textTheme: const TextTheme(
-                  bodySmall: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18.0,
-                    color: Colors.white,
-                  )
-              ),
-              appBarTheme: AppBarTheme(
-                  titleTextStyle: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  iconTheme: const IconThemeData(
-                    color: Colors.white,
-                  ),
-                  backgroundColor: HexColor('333739'),
-                  elevation: 0.0,
-                  systemOverlayStyle: SystemUiOverlayStyle(
-                    statusBarColor: HexColor('333739'),
-                    statusBarIconBrightness: Brightness.light,
-                  )
-              ),
-              bottomNavigationBarTheme: BottomNavigationBarThemeData(
-                backgroundColor: HexColor('333739'),
-                type: BottomNavigationBarType.fixed,
-                showUnselectedLabels: true,
-                showSelectedLabels: true,
-                elevation: 8.0,
-                selectedItemColor: Colors.white,
-                unselectedItemColor: Colors.grey,
-              ),
-            ),
+            theme: lightTheme,
+            darkTheme: darkTheme,
             themeMode: NewsCubit.get(context).isDark? ThemeMode.dark : ThemeMode.light,
             home: const NewsLayout(),
 
